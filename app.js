bbec204a71e911980032b510cf626a483d9eb7d8
Christopher Backes <you@example.com>
2022-01-16 03:01:09 -0500

    lesson 2: importing/exporting and creating documents

--- app.js
+++ app.js
@@ -1,4 +1,11 @@
-const profileDataArgs = process.argv.slice(2, process.argv.length);
+const fs = require('fs')
+const generatePage = require('./src/page-template.js')
 
-profileDataArgs.forEach(profileItem => console.log(profileItem))
+const profileDataArgs = process.argv.slice(2);
+const [profName, github] = profileDataArgs;
 
+  fs.writeFile('index.html', generatePage(profName, github), err => {
+    if (err) throw err;
+  
+    console.log('Portfolio complete! Check out index.html to see the output!');
+  });
